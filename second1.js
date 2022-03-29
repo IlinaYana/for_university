0c25594a19dd9969c1c594f3176c851268e97cf0
IlinaYana <99615684+IlinaYana@users.noreply.github.com>
2022-03-30 01:35:03 +0300

    -

--- second1.js
+++
@@ -1,7 +0,0 @@
-//Массив строк
-var stringArray = ["Поешь", "еще", "этих", "мягких", "французских", "булок"];
-var str = "";
-for (element of stringArray){
-    str = str+element+" ";
-}
-console.log(str);
