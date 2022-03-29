54acb4af2c1fa54b602cda71f453d5d1bc5f3503
IlinaYana <20000808@edu.spbgasu.ru>
2022-03-30 00:57:54 +0300

    add homework 27.03

---
+++ second1.js
@@ -0,0 +1,7 @@
+//Массив строк
+var stringArray = ["Поешь", "еще", "этих", "мягких", "французских", "булок"];
+var str = "";
+for (element of stringArray){
+    str = str+element+" ";
+}
+console.log(str);
